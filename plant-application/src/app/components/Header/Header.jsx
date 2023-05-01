4e2413286a8ba68cca992102c1f5d9356275a8d4
Jon Jackson <jonjackson.webdesign@gmail.com>
2023-04-30 21:52:12 -0500

    imain page is looking pretty good

--- plant-application/src/app/components/Header/Header.jsx
+++ plant-application/src/app/components/Header/Header.jsx
@@ -1,20 +1,28 @@
 import Link from "next/link";
+import Search from "../Search/Search";
 import styles from "./header.module.css";
 
 const Header = () => {
   return (
     <div>
-      <div className={styles.pictureGreetingContainer}>
-        <img
-          className={styles.profilePicture}
-          src="/images/Ben.jpeg"
-          alt="profile"
-        />
-        <p className={styles.greeting}>
-          Hi, <span className="username">Ben!</span>
-        </p>
+      <div className={styles.headerComponents}>
+        <div className={styles.memberInfo}>
+          <div className={styles.pictureGreetingContainer}>
+            <img
+              className={styles.profilePicture}
+              src="/images/Ben.jpeg"
+              alt="profile"
+            />
+            <p className={styles.greeting}>
+              Hi, <span className="username">Ben!</span>
+            </p>
+          </div>
+          <p className={styles.flowerCount}>4 Flowers</p>
+        </div>
+        <div className={styles.searchContainer}>
+        <Search />
+        </div>
       </div>
-      <p className={styles.flowerCount}>4 Flowers</p>
       <ul className="homepage-nav">
         <li className="homepage-nav-item">
           <Link className={styles.link} href="/">
--- plant-application/src/app/components/Search/Search.jsx
+++ plant-application/src/app/components/Search/Search.jsx
@@ -1,7 +1,15 @@
-
+import {BsSearch} from 'react-icons/Bs';
+import styles from './search.module.css'
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className={styles.searchContainer}>
+      <BsSearch className={styles.searchIcon} />
+      <input
+        placeholder="Search"
+        type="text"
+        className={styles.searchInput}
+      />
+    </div>
   )
 }
 
